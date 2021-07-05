7cf3bd72e1fd9b086bec82aea09041d1677e89ea
root <root@ip-172-31-46-191.us-east-2.compute.internal>
2021-07-05 13:13:37 +0000

    code

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+	ini_set('display_errors',1);
+	error_reporting(-1);
+	define('DB_HOST', 'idukan.cdvchkqdm7k4.us-east-2.rds.amazonaws.com');
+    define('DB_USER', 'admin');
+    define('DB_PASSWORD', 'pravin561');
+    define('DB_DATABASE', 'idukan');
+?>
